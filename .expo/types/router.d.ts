a5e9a2295c82b83e5dfec88feed71ec06510928e
abdulvohid31 <abdulvohidcode@gmail.com>
2024-09-13 15:29:00 +0500

    sac

--- .expo/types/router.d.ts
+++ .expo/types/router.d.ts
@@ -6,7 +6,7 @@ export * from 'expo-router';
 declare module 'expo-router' {
   export namespace ExpoRouter {
     export interface __routes<T extends string = string> extends Record<string, unknown> {
-      StaticRoutes: `/` | `/(client)` | `/(client)/` | `/(client)/(dashboard)` | `/(client)/client` | `/(client)/explore` | `/(dashboard)` | `/(master)` | `/(master)/` | `/(master)/explore` | `/(master)/master` | `/(pages)` | `/(pages)/(client)` | `/(pages)/(client)/(dashboard)` | `/(pages)/(client)/client` | `/(pages)/(dashboard)` | `/(pages)/(master)/master` | `/(pages)/client` | `/(pages)/master` | `/(tabs)` | `/(tabs)/` | `/(tabs)/(client)` | `/(tabs)/(client)/` | `/(tabs)/(client)/explore` | `/(tabs)/(master)` | `/(tabs)/(master)/` | `/(tabs)/(master)/explore` | `/(tabs)/explore` | `/..\components\navigation\NavigationMenu` | `/..\components\navigation\TabBarIcon` | `/..\types\navigation\navigation` | `/_sitemap` | `/client` | `/explore` | `/master`;
+      StaticRoutes: `/` | `/(client)` | `/(client)/` | `/(client)/(dashboard)` | `/(client)/client` | `/(client)/explore` | `/(dashboard)` | `/(master)` | `/(master)/` | `/(master)/explore` | `/(master)/master` | `/(pages)` | `/(pages)/(client)` | `/(pages)/(client)/(dashboard)` | `/(pages)/(client)/client` | `/(pages)/(dashboard)` | `/(pages)/(master)/master` | `/(pages)/client` | `/(pages)/master` | `/(tabs)` | `/(tabs)/` | `/(tabs)/(client)` | `/(tabs)/(client)/` | `/(tabs)/(client)/explore` | `/(tabs)/(master)` | `/(tabs)/(master)/` | `/(tabs)/(master)/explore` | `/(tabs)/explore` | `/..\components\navigation\NavigationMenu` | `/..\layout\layout` | `/_sitemap` | `/client` | `/explore` | `/master`;
       DynamicRoutes: never;
       DynamicRouteTemplate: never;
     }
