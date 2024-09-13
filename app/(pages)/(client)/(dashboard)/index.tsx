0d7c8949d8513f0177e1d869294da8b79929fee5
abdulazizsaidovcode <abdulazizbey5@gmail.com>
2024-09-13 11:40:33 +0500

    boldi chogimda

---
+++ app/(pages)/(client)/(dashboard)/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const DashboardClient = () => {
+  return (
+    <View>
+      <Text>DashboardClient</Text>
+    </View>
+  )
+}
+
+export default DashboardClient
