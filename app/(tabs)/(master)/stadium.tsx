60afb2498225a00335e6f7b9c01546a74675ce2c
abdulazizsaidovcode <abdulazizbey5@gmail.com>
2024-09-18 14:19:16 +0500

    push

--- app/(tabs)/(master)/stadium.tsx
+++ app/(tabs)/(master)/stadium.tsx
@@ -1,12 +1,9 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import Stadium from '@/app/(pages)/(master)/(stadium)/stadium'
 
 const MasterStadiumScreen = () => {
-  return (
-    <View>
-      <Text>MasterStadiumScreen</Text>
-    </View>
-  )
+  return <Stadium />
 }
 
 export default MasterStadiumScreen
