395f55b1ed30ade696522fe08c07b30ab48ddd54
abdulazizsaidovcode <abdulazizbey5@gmail.com>
2024-09-25 12:00:09 +0500

    vb

--- android/app/src/main/java/uz/sfera/stadium/MainApplication.kt
+++ android/app/src/main/java/uz/sfera/stadium/MainApplication.kt
@@ -1,62 +1,40 @@
 package uz.sfera.stadium
 
 import android.app.Application
-import android.content.res.Configuration
-import com.airbnb.lottie.LottiePackage; // Lottie importi
-
-import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
 import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
-import com.facebook.react.ReactHost
-import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
-import com.facebook.react.defaults.DefaultReactNativeHost
+import com.facebook.react.shell.MainReactPackage
 import com.facebook.soloader.SoLoader
-
-import expo.modules.ApplicationLifecycleDispatcher
-import expo.modules.ReactNativeHostWrapper
+import com.airbnb.android.react.lottie.LottiePackage
+import java.util.List
 
 class MainApplication : Application(), ReactApplication {
-  @Override
-    protected List<ReactPackage> getPackages() {
-        return Arrays.<ReactPackage>asList(
-            new MainReactPackage(),
-            new LottiePackage() // Lottie package qo'shish
-        );
+
+    private val mReactNativeHost: ReactNativeHost = object : ReactNativeHost(this) {
+        override fun getUseDeveloperSupport(): Boolean {
+            return BuildConfig.DEBUG
+        }
+
+        override fun getPackages(): List<ReactPackage> {
+            // Bu yerda paketlaringizni ro'yxatlang
+            return listOf(
+                MainReactPackage(),
+                LottiePackage() // Lottie animatsiyalari uchun paket
+            )
+        }
+
+        override fun getJSMainModuleName(): String {
+            return "index"
+        }
     }
-  override val reactNativeHost: ReactNativeHost = ReactNativeHostWrapper(
-        this,
-        object : DefaultReactNativeHost(this) {
-          override fun getPackages(): List<ReactPackage> {
-            // Packages that cannot be autolinked yet can be added manually here, for example:
-            // packages.add(new MyReactNativePackage());
-            return PackageList(this).packages
-          }
 
-          override fun getJSMainModuleName(): String = ".expo/.virtual-metro-entry"
-
-          override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
-
-          override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
-          override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
-      }
-  )
-
-  override val reactHost: ReactHost
-    get() = ReactNativeHostWrapper.createReactHost(applicationContext, reactNativeHost)
-
-  override fun onCreate() {
-    super.onCreate()
-    SoLoader.init(this, false)
-    if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
-      // If you opted-in for the New Architecture, we load the native entry point for this app.
-      load()
+    override fun getReactNativeHost(): ReactNativeHost {
+        return mReactNativeHost
     }
-    ApplicationLifecycleDispatcher.onApplicationCreate(this)
-  }
 
-  override fun onConfigurationChanged(newConfig: Configuration) {
-    super.onConfigurationChanged(newConfig)
-    ApplicationLifecycleDispatcher.onConfigurationChanged(this, newConfig)
-  }
+    override fun onCreate() {
+        super.onCreate()
+        SoLoader.init(this, /* native exopackage */ false)
+    }
 }
