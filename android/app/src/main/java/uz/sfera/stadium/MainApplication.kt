f23aab60c03ba030770a4fa1378368db37cbe995
Behruz1227 <behruzxonniyozov12@gmail.com>
2024-09-25 13:19:50 +0500

    m,kmk

--- android/app/src/main/java/uz/sfera/stadium/MainApplication.kt
+++ android/app/src/main/java/uz/sfera/stadium/MainApplication.kt
@@ -1,40 +1,55 @@
-package uz.sfera.stadium
+package uz.sfera.com
 
 import android.app.Application
+import android.content.res.Configuration
+import uz.sfera.stadium.BuildConfig
+import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
 import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
-import com.facebook.react.shell.MainReactPackage
+import com.facebook.react.ReactHost
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
+import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.soloader.SoLoader
-import com.airbnb.android.react.lottie.LottiePackage
-import java.util.List
+
+import expo.modules.ApplicationLifecycleDispatcher
+import expo.modules.ReactNativeHostWrapper
 
 class MainApplication : Application(), ReactApplication {
 
-    private val mReactNativeHost: ReactNativeHost = object : ReactNativeHost(this) {
-        override fun getUseDeveloperSupport(): Boolean {
-            return BuildConfig.DEBUG
-        }
+  override val reactNativeHost: ReactNativeHost = ReactNativeHostWrapper(
+        this,
+        object : DefaultReactNativeHost(this) {
+          override fun getPackages(): List<ReactPackage> {
+            // Packages that cannot be autolinked yet can be added manually here, for example:
+            // packages.add(new MyReactNativePackage());
+            return PackageList(this).packages
+          }
 
-        override fun getPackages(): List<ReactPackage> {
-            // Bu yerda paketlaringizni ro'yxatlang
-            return listOf(
-                MainReactPackage(),
-                LottiePackage() // Lottie animatsiyalari uchun paket
-            )
-        }
+          override fun getJSMainModuleName(): String = ".expo/.virtual-metro-entry"
 
-        override fun getJSMainModuleName(): String {
-            return "index"
-        }
+          override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
+
+          override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
+          override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
+      }
+  )
+
+  override val reactHost: ReactHost
+    get() = ReactNativeHostWrapper.createReactHost(applicationContext, reactNativeHost)
+
+  override fun onCreate() {
+    super.onCreate()
+    SoLoader.init(this, false)
+    if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
+      // If you opted-in for the New Architecture, we load the native entry point for this app.
+      load()
     }
+    ApplicationLifecycleDispatcher.onApplicationCreate(this)
+  }
 
-    override fun getReactNativeHost(): ReactNativeHost {
-        return mReactNativeHost
-    }
-
-    override fun onCreate() {
-        super.onCreate()
-        SoLoader.init(this, /* native exopackage */ false)
-    }
+  override fun onConfigurationChanged(newConfig: Configuration) {
+    super.onConfigurationChanged(newConfig)
+    ApplicationLifecycleDispatcher.onConfigurationChanged(this, newConfig)
+  }
 }
